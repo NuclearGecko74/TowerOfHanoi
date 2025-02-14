db11668aec852f4d3ea99f5bcbfe04907382bbeb
Anton Olguin <antonolguin1@gmail.com>
2025-02-13 23:22:10 -0600

    Add project files.

---
+++ Pila.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTower
+{
+    class Pila
+    {
+        private Node top;
+        private int height;
+
+        public Pila(int data)
+        {
+            top = new Node(data);
+            height = 1;
+        }
+
+        public Pila()
+        {
+            top = null;
+            height = 0;
+        }
+
+        public void Push(int data)
+        {
+            if (IsEmpty())
+            {
+                top = new Node(data);
+            }
+            else
+            {
+                top = new Node(data, top);
+            }
+            height++;
+        }
+
+        public Node Pop()
+        {
+            if (IsEmpty()) return null;
+
+            Node temp = top;
+            top = top.next;
+            return temp;
+        }
+
+        public int Peek() { return top.data; }
+
+        public void PrintStack()
+        {
+            Node current = top;
+            while(current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+        }
+
+        bool IsEmpty() { return top == null;  }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HanoiTower;
+using Raylib_cs;
+
+class Program
+{
+    static void Main()
+    {
+        //Raylib.InitWindow(800, 600, "Raylib en C#");
+        //Raylib.SetTargetFPS(60);
+
+        //while (!Raylib.WindowShouldClose())
+        //{
+        //    Raylib.BeginDrawing();
+        //    Raylib.ClearBackground(Color.Black);
+        //    Raylib.DrawText("¡Hola, Raylib en C#!", 200, 250, 20, Color.White);
+        //    Raylib.EndDrawing();
+        //}
+        //Raylib.CloseWindow();
+        Pila stack = new Pila(2);
+        stack.Push(1);
+        stack.Push(0);
+
+        stack.Pop();
+
+        Console.WriteLine(stack.Peek());
+    }
+}
