470f3b2b4c6c2ec437319bea454fe6b056430f7f
Anton Olguin <antonolguin1@gmail.com>
2025-02-15 14:13:39 -0600

    Basic window

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,23 +1,11 @@
 ﻿using System;
 using HanoiTower.Source.DataStructures;
 using HanoiTower.Source.Renderer;
-using Raylib_cs;
 
 class Program
 {
     static void Main()
     {
-        //Raylib.InitWindow(800, 600, "Torres de Hanoi");
-        //Raylib.SetTargetFPS(60);
-
-        //while (!Raylib.WindowShouldClose())
-        //{
-        //    Raylib.BeginDrawing();
-        //    Raylib.ClearBackground(Color.Black);
-        //    Raylib.DrawText("Hola causas pes", 200, 250, 20, Color.White);
-        //    Raylib.EndDrawing();
-        //}
-        //Raylib.CloseWindow();
         Game game = new Game();
         game.Start();
 
--- Source/Renderer/Game.cs
+++ Source/Renderer/Game.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Raylib_cs;
+
 namespace HanoiTower.Source.Renderer
 {
     class Game
     {
+        private const short WIDTH = 800;
+        private const short HEIGHT = 600;
+
+        public Game()
+        {
+            Raylib.InitWindow(WIDTH, HEIGHT, "Torres de Hanoi");
+            Raylib.SetTargetFPS(60);
+        }
+
         public void Start()
         {
-
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                Raylib.DrawText("Hello World!", 200, 250, 20, Color.White);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
         }
     }
 }
